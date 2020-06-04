daf99fc4b2596ec36bee64f23746e872eaa38c02
AlvaroMarques <sc.poi458@gmail.com>
2020-06-04 14:42:18 -0300

    penis penis penis

--- sandbox/operationToArm.c
+++ sandbox/operationToArm.c
@@ -18,9 +18,9 @@ typedef struct Operation {
 	Operator operator;
 } Operation; 
 
-Operation * createHead(Operator op){
+Operation * createHead(int value, Operator op){
 	Operation * head = malloc(sizeof(Operation));
-	head->value = -1;
+	head->value = value;
 	head->next=NULL;
 	head->previous=NULL;
 	head->solving=NULL;
@@ -50,136 +50,6 @@ Operation * addOperation(Operation *head, int value, Operator op){
 	return operation;
 }   
 
-typedef struct NumberList {
-	struct NumberList * next;
-	struct NumberList * previous;
-	int value;
-} NumberList;
-
-NumberList * createNumberPointer(){
-	NumberList * head = malloc(sizeof(NumberList));
-	head->value = -1;
-	head->previous = NULL;
-	head->next=NULL;
-	return head;
-}
-
-void addToNumberList(NumberList * head, int value){
-	NumberList * new = malloc(sizeof(NumberList));	
-	new->value  = value;
-	new->previous = head->previous;
-	if (head->previous != NULL)
-		head->previous->next = new;
-	head->previous = new;
-}
-
-int detachValueNumberList (NumberList * head){
-	NumberList * freedom = head->previous;
-	int tmp = freedom->value;
-	head->previous = freedom->previous;
-	free(freedom);
-	return tmp;
-}
-
-int gotoNextNumberList(NumberList *head){
-	if (head == NULL) return -1;
-	if (head->next == NULL) return -1;
-	int ret = head->next->value; 
-	head->previous = head->next;
-	head->next = head->next->next;
-	return ret;
-}
-
-int gotoPreviousNumberList(NumberList *head){
-	if (head == NULL) return EOE;
-	int ret = head->previous->value; 
-	head->next = head->previous;
-	head->previous = head->previous->previous;
-	return ret;
-}
-
-
-typedef struct OperatorList {
-	struct OperatorList * next;
-	struct OperatorList * previous;
-	int value;
-} OperatorList;
-
-OperatorList * createOperatorPointer(){
-	OperatorList * head = malloc(sizeof(OperatorList));
-	head->value = EOE;
-	head->previous = NULL;
-	head->next=NULL;
-	return head;
-}
-
-void addToOperatorList(OperatorList * head, Operator value){
-	OperatorList * new = malloc(sizeof(OperatorList));	
-	new->value  = value;
-	new->previous = head->previous;
-	if (head->previous != NULL)
-		head->previous->next = new;
-	head->previous = new;
-}
-
-Operator gotoNextOperatorList(OperatorList *head){
-	if (head == NULL) return EOE;
-	if (head->next == NULL) return EOE;
-	Operator ret = head->next->value; 
-	head->previous = head->next;
-	head->next = head->next->next;
-	return ret;
-}
-
-Operator gotoPreviousOperatorList(OperatorList *head){
-	if (head == NULL) return EOE;
-	Operator ret = head->previous->value; 
-	head->next = head->previous;
-	head->previous = head->previous->previous;
-	return ret;
-}
-
-int detachValueOperatorList (OperatorList * head){
-	OperatorList * freedom = head->previous;
-	Operator tmp = freedom->value;
-	head->previous = freedom->previous;
-	free(freedom);
-	return tmp;
-}
-
-void showExpression(Operation * head){
-	if (head == NULL) return;
-	if (head->value != -1) // parentese
-		printf("%d ", head->value);
-	if (head->solving != NULL){
-		printf("(");
-		showExpression(head->solving);
-		printf(")");
-	}
-	switch (head->operator){
-		case ADD:
-			printf(" + ");
-			break;
-		case MUL:
-			printf(" * ");
-			break;
-		case DIV:
-			printf(" / ");
-			break;
-		case EXP:
-			printf(" ^ ");
-			break;
-		case EOE:
-			printf(";");
-			break;
-		default:
-			printf("..");
-			break;
-	}
-	showExpression(head->next);
-}
-
-
 void printOperator(Operator operator){
 	switch (operator){
 		case ADD:
@@ -202,59 +72,94 @@ void printOperator(Operator operator){
 			break;
 	}
 }
-void printOperatorList(OperatorList *start){
-	if (start->previous == NULL) {
-		printf("\n");
-		return;
-	}
-	printOperator(start->previous->value);
-	printOperatorList(start->previous);
 
+void showExpression(Operation * head){
+	if (head == NULL) return;
+	if (head->value != -1) // parentese
+		printf("%d ", head->value);
+	if (head->solving != NULL){
+		printf("(");
+		showExpression(head->solving);
+		printf(")");
+	}
+	printOperator(head->operator);
+	showExpression(head->next);
 }
 
+
+
+
 int solveExpression(Operation * head){
-	NumberList * headNumber = createNumberPointer();
-	OperatorList * headOperator = createOperatorPointer();
-	if (head->solving != NULL){
-		head->value = solveExpression(head->solving);
-	}
+	Operation *expression = NULL;
+	Operation *aux;
 	Operation *p;
 	for (p = head; p != NULL; p = p->next){
 		if (p->solving != NULL){
 			p->value = solveExpression(p->solving);
 		}	
-		addToNumberList(headNumber,p->value);
-		addToOperatorList(headOperator,p->operator);
+		if (p == head){
+			expression = createHead(head->value, head->operator);
+			aux = expression;
+		}else{
+			expression = addOperation(expression,p->value,p->operator);
+		}
 	}
 	int init = 0;
-	int tmp;
-	Operator optmp;
-	while (headNumber->previous->previous != NULL){
-		tmp = gotoPreviousNumberList(headNumber);
-		optmp = gotoPreviousOperatorList(headOperator);
-		printOperatorList(debug);
-		if (optmp == EXP) {
-			headNumber->previous->value = pow(headNumber->previous->value, tmp);
-			headNumber->previous->next->value = 1;
-			headOperator->previous->value = MUL;  // trocando A ^ Y por (A^Y) x 1
-		}		
+	printf("expression [before exp]: ");
+	showExpression(aux);
+	printf("\n");
 
+	for (p = expression; p != NULL; p = p->previous){
+		if (p->previous != NULL && p->previous->operator == EXP) {
+			p->previous->value = pow(p->previous->value, p->value);
+			p->previous->operator = MUL;
+			p->value = 1;
+		}
 	}
-	
+	printf("expression [after exp]: ");
+	showExpression(aux);
+	printf("\n");
+	for (p = aux ; p != NULL; p = p->next){
+		if (p->next != NULL && p->operator == MUL) {
+			p->next->value = p->next->value * p->value;
+			p->operator = ADD;
+			p->value = 0;
+		} else if (p->next != NULL && p->operator == DIV) {
+			p->next->value = p->value/p->next->value;
+			p->operator = ADD;
+			p->value = 0;
+		}
+	}
+	printf("expression [after mul]: ");
+	showExpression(aux);
+	printf("\n");
+	int return_value = 0;
+	for (p = expression; p != NULL; p = p->previous){
+		if (p->previous != NULL && p->previous->operator == ADD) {
+			p->previous->value = p->previous->value + p->value;
+			return_value = p->previous->value;
+			p->value = 0;
+		}
+	}
+	printf("expression [after add]: ");
+	showExpression(aux);
+	printf("\n");
 	
 
 	
-	return 0;
+	return return_value;
 }
 
 
 int main(){
-	Operation * o = createHead(EOE);
-	Operation *p;
-	p = addOperationSolving(o, 3, MUL);  
-	p = addOperation(p, 5, EXP);  
-	p = addOperation(p, 2, EOE);  
-
+	Operation * o = createHead(-1,MUL);
+	Operation *p, *q;
+	q = addOperationSolving(o, 2, ADD);
+	q = addOperation(q, 5, EOE);
+	p = addOperation(o, 3, EXP);
+	q = addOperation(p, -1, EOE);  
+	q = addOperationSolving(q, 6, DIV);
+      	q = addOperation(q,2, EOE);	
 	showExpression(o);
 	printf("\n");
 	printf("resultado: %d\n",solveExpression(o));
