40476e17196405b0d49342a5bf62c26e7486b85f
Victor Toon <araujo.victor200@gmail.com>
2020-06-04 18:07:28 -0300

    NOVA ORDEM MUNDIAL
    entao, parece que tá funcionando tudo certinha as contas com as listas ligadas, ficou bullet demais

--- src/operation.c
+++ src/operation.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 #include "operation.h"
 
 operacoes *criaLista(int valor, operadores op){
@@ -27,20 +28,30 @@ operacoes *populaprox(int valor, operadores op, operacoes *cabeca, int flag){
 int resolveShiftReduce(operacoes *cabeca){
     if (cabeca->num == -1){
         cabeca->num = resolveShiftReduce(cabeca->cima);
-        resolveShiftReduce(cabeca);
+        return resolveShiftReduce(cabeca);
     }
     else{
+        if (cabeca->dir != NULL && cabeca->dir->num == -1){
+            cabeca->dir->num = resolveShiftReduce(cabeca->dir->cima);
+        }
         if (cabeca->op == ADD){
             if (cabeca->dir->op == ADD){
                 cabeca->dir->num = cabeca->dir->num + cabeca->num;
+                cabeca->num = 0;
+                return resolveShiftReduce(cabeca->dir);
             }
-            if (cabeca->dir->op == MULT || cabeca->dir->op == DIV)
-            {
+            if (cabeca->dir->op == MULT || cabeca->dir->op == DIV){
                 resolveShiftReduce(cabeca->dir);
-                resolveShiftReduce(cabeca);
+                return resolveShiftReduce(cabeca);
             }
             if (cabeca->dir->op == EXP){
-                
+                resolveShiftReduce(cabeca->dir);
+                return resolveShiftReduce(cabeca);
+            }
+            if (cabeca->dir->op == EOE){
+                cabeca->dir->num = cabeca->dir->num + cabeca->num;
+                cabeca->num = 0;
+                return cabeca->dir->num;
             }
         }
 
@@ -49,14 +60,23 @@ int resolveShiftReduce(operacoes *cabeca){
                 cabeca->dir->num = cabeca->num * cabeca->dir->num;
                 cabeca->num = 0;
                 cabeca->op = ADD;
+                return resolveShiftReduce(cabeca->dir);
             }
             if (cabeca->dir->op == MULT || cabeca->dir->op == MULT){
                 cabeca->dir->num = cabeca->num * cabeca->dir->num;
                 cabeca->num = 0;
                 cabeca->op = ADD;
+                return resolveShiftReduce(cabeca->dir);
             }
             if (cabeca->dir->op == EXP){
-
+                resolveShiftReduce(cabeca->dir);
+                return resolveShiftReduce(cabeca);
+            }
+            if (cabeca->dir->op == EOE){
+                cabeca->dir->num = cabeca->num * cabeca->dir->num;
+                cabeca->num = 0;
+                cabeca->op = ADD;
+                return cabeca->dir->num;
             }
         }
         else if (cabeca->op == DIV){
@@ -64,26 +84,55 @@ int resolveShiftReduce(operacoes *cabeca){
                 cabeca->dir->num = cabeca->num / cabeca->dir->num;
                 cabeca->num = 0;
                 cabeca->op = ADD;
+                return resolveShiftReduce(cabeca->dir);
             }
             if (cabeca->dir->op == MULT || cabeca->dir->op == MULT){
                 cabeca->dir->num = cabeca->num / cabeca->dir->num;
                 cabeca->num = 0;
                 cabeca->op = ADD;
+                return resolveShiftReduce(cabeca->dir);
             }
-            if (cabeca->dir->op == EXP)
-            {
+            if (cabeca->dir->op == EXP){
+                resolveShiftReduce(cabeca->dir);
+                return resolveShiftReduce(cabeca);
+            }
+            if (cabeca->dir->op == EOE){
+                cabeca->dir->num = cabeca->num / cabeca->dir->num;
+                cabeca->num = 0;
+                cabeca->op = ADD;
+                return cabeca->dir->num;
             }
         }
 
         else if (cabeca->op == EXP){
             if (cabeca->dir->op == ADD){
+                cabeca->dir->num = pow(cabeca->num, cabeca->dir->num);
+                cabeca->num = 1;
+                cabeca->op = MULT;
+                return resolveShiftReduce(cabeca->dir);
             }
-            if (cabeca->dir->op == MULT){
+            if (cabeca->dir->op == MULT || cabeca->dir->op == MULT){
+                cabeca->dir->num = pow(cabeca->num, cabeca->dir->num);
+                cabeca->num = 1;
+                cabeca->op = MULT;
+                return resolveShiftReduce(cabeca->dir);
             }
             if (cabeca->dir->op == EXP){
+                resolveShiftReduce(cabeca->dir);
+                return resolveShiftReduce(cabeca);
+            }
+            if (cabeca->dir->op == EOE){
+                cabeca->dir->num = pow(cabeca->num, cabeca->dir->num);
+                cabeca->num = 1;
+                cabeca->op = MULT;
+                return cabeca->dir->num;
             }
         }
+        else if (cabeca->op == EOE){
+            return cabeca->num;
+        }
     }
+    return -2;
 }
 
 
@@ -103,7 +152,7 @@ char printaOperador(operadores op){
         return '^';
         break;
     default:
-        return ';';
+        return ' ';
         break;
     }
 }
@@ -128,14 +177,26 @@ void printaTudo(operacoes *cabeca){
 }
 
 int main(){
+    // operacoes* conta = criaLista(1, ADD);
+    // operacoes* auxConta = populaprox(1, ADD, conta, direita);
+    // auxConta = populaprox(1, EOE, auxConta, direita);
+
     operacoes* conta = criaLista(-1, MULT);
     operacoes* auxConta = populaprox(2, ADD, conta, parentese);
     auxConta = populaprox(5, EOE, auxConta, direita);
     auxConta = populaprox(-1, EOE, conta, direita);
     auxConta = populaprox(3, EXP, auxConta, parentese);
     auxConta = populaprox(2, EOE, auxConta, direita);
+
+    // operacoes* conta = criaLista(-1, ADD);
+    // operacoes* auxConta = populaprox(1, EOE, conta, parentese);
+    // auxConta = populaprox(-1, EOE, conta, direita);
+    // auxConta = populaprox(1, EOE, auxConta, parentese);
+    
+
     printaTudo(conta);
     printf("\n");
+    printf("%d\n", resolveShiftReduce(conta));
 
     return 0;
 }
